9f0ae99c8bff2cbfe0c1314b93f2d05d74c45979
David Kinder <david.kinder@david.kinder>
2021-05-23 19:38:53 +0100

    Fix direct threading for 64-bit

--- terp.c
+++ terp.c
@@ -107,6 +107,11 @@ void startProgram (size_t cacheSize, enum IOMode ioMode)
 #include "labels.inc"
     NULL};
     gOpcodeTable = opcodeTable;
+# if (UINTPTR_MAX > 0xffffffffULL)
+    const uintptr_t opcodeHi = (uintptr_t)opcodeTable[0] & ~0xffffffffULL;
+    for (L1 = 1; opcodeTable[L1] != NULL; ++L1)
+      assert (opcodeHi == ((uintptr_t)opcodeTable[L1] & ~0xffffffffULL));
+# endif
 #endif    
 
     initCompiler (cacheSize);
@@ -129,7 +134,9 @@ void startProgram (size_t cacheSize, enum IOMode ioMode)
     L2 = 0; // No arguments.
     goto do_enter_function_L1;
 
-#ifdef USE_DIRECT_THREADING
+#if defined(USE_DIRECT_THREADING) && (UINTPTR_MAX > 0xffffffffULL)
+#define NEXT do { goto *(Opcode)(*(pc++) | opcodeHi); } while(0)
+#elif defined(USE_DIRECT_THREADING)
 #define NEXT do { goto **(pc++); } while(0)
 #else
 #define NEXT goto next
